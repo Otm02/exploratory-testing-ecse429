5f759f1e7edf8477df467dc1f714288fbff64aa0
Athmane Benarous <30377400+Otm02@users.noreply.github.com>
2024-11-03 21:58:11 -0500

    add shuffling

--- partB/features/environment.py
+++ partB/features/environment.py
@@ -1,4 +1,13 @@
 from features.steps.utils import reset_database_to_default
+import random
 
-def after_scenario(context, scenario):
+def before_all(context):
+    """Shuffle features before execution starts."""
+    random.shuffle(context._runner.features)
+
+def before_feature(context, feature):
+    """Shuffle scenarios within each feature."""
+    random.shuffle(feature.scenarios)
+
+def after_feature(context, scenario):
     reset_database_to_default()
